2143d6f94d4b21ad76fc61631d10939b31c581ec
Natalia Chueco <natchueco@gmail.com>
2022-12-06 20:57:55 +0100

    first iteration components

---
+++ lab-app/src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<template>
+  <Navbar/>
+  <div class="main-content">
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <Footer/>
+</template>
+
+<style scoped>
+.main-content{
+  display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
